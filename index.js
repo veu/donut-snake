3ba6d1a49454d1d6f2c2671bec9d6a4049b38273
Becky <becky@monometric.net>
2017-12-08 19:21:48 +0100

    shrink and wrap grid

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const getNeighbors = (midX, midY) => {
 
     for (let x = -1; x < 2; x++) {
         for (let y = -1; y < 2; y++) {
-            if ((x || y) && grid[x + midX + (y + midY) * 6] !== undefined) {
-                neighbors.push(grid[x + midX + (y + midY) * 6]);
+            const index = (x + midX + 5) % 5 + (y + midY + 5) % 5 * 5;
+            if ((x || y) && grid[index] !== undefined) {
+                neighbors.push(grid[index]);
             }
         }
     }
@@ -25,34 +26,32 @@ const getRandomColor = (x, y) => {
 };
 
 const grid = [];
-for(i = 6 * 8; i--;){
-  grid[i] = getRandomColor(i % 6, i / 6 | 0);
+for(i = 25; i--;){
+  grid[i] = getRandomColor(i % 5, i / 5 | 0);
 }
 
 const snake = {
     colors: [],
-    positions: [{x: 5, y: 6}, {x: 5, y: 7}]
+    positions: [{x: 2, y: 2}, {x: 2, y: 3}]
 };
-
-grid[41] = grid[47] = 0;
 
 let moves = 8;
 let score = 0;
 const c = a.getContext('2d');
 
-localStorage.hs = localStorage.hs || 0;
+localStorage.hs2 = localStorage.hs2 || 0;
 
 draw = e => {
     a.width = innerWidth;
     a.height = innerHeight;
 
-    const scale = Math.min(innerWidth / 140, innerHeight / 220);
+    const scale = Math.min(innerWidth / 120, innerHeight / 170);
     c.scale(scale, scale);
     c.translate(10,10);
 
-    for(y=8;y--;)
-        for(x=6;x--;) {
-            const value = grid[x + y * 6];
+    for(y=5;y--;)
+        for(x=5;x--;) {
+            const value = grid[x + y * 5];
             c.fillStyle = c.strokeStyle = ['#f00','#0f0','#00f','#ee0'][value % 4];
             if (value < 4) c.fillRect(x*20+5,y*20+5,10,10);
             else c.strokeRect(x*20+4,y*20+4,12,12);
@@ -85,12 +84,14 @@ draw = e => {
 
     c.font = '12px Arial';
     c.fillStyle = moves < 5 ? '#d00' : '#000';
-    c.fillText('MOVES ' + moves, 2, 180);
+    c.fillText('MOVES ' + moves, 2, 120);
     c.fillStyle = '#000';
-    c.fillText('SCORE ' + score + ' / ' + localStorage.hs, 2, 199);
+    c.fillText('SCORE ' + score + ' / ' + localStorage.hs2, 2, 139);
 }
 
 draw();
+
+onresize = () => draw();
 
 onkeydown = e => {
   if (37 <= e.keyCode && e.keyCode <= 40) {
@@ -124,26 +125,26 @@ move = async key => {
     if (key === 3) dir = {x: 0, y: 1};
 
     const part = {
-        x: snake.positions[0].x + dir.x,
-        y: snake.positions[0].y + dir.y,
+        x: (snake.positions[0].x + dir.x + 5) % 5,
+        y: (snake.positions[0].y + dir.y + 5) % 5,
     };
 
-    if (0 > part.x || part.x > 5 || 0 > part.y || part.y > 7 || isOccupied(part)) {
+    if (isOccupied(part)) {
         return;
     }
 
-    const value = grid[part.x + part.y * 6];
+    const value = grid[part.x + part.y * 5];
 
     snake.positions.unshift(part);
 
     if (value < 4) {
         snake.colors.unshift(value);
-        grid[part.x + part.y * 6] = getRandomColor(part.x, part.y);
+        grid[part.x + part.y * 5] = getRandomColor(part.x, part.y);
     } else {
         snake.positions.pop();
         const delta = digest(value % 4);
         score += delta * (delta + 1) / 2;
-        if (score > localStorage.hs) localStorage.hs = score;
+        if (score > localStorage.hs2) localStorage.hs2 = score;
         moves += delta * 2;
     }
 
@@ -180,7 +181,7 @@ removeColor = color => {
 
     for (let k = numRemoved; k--;) {
         const pos = snake.positions.pop();
-        grid[pos.x + pos.y * 6] = getRandomColor(pos.x, pos.y);
+        grid[pos.x + pos.y * 5] = getRandomColor(pos.x, pos.y);
     }
 
     return numRemoved;
