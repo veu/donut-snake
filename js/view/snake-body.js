82817be3f1763dfe30b33e7d3765f943b07cc6d1
Becky <becky@monometric.net>
2017-12-12 23:15:25 +0100

    refactor

--- js/view/snake-body.js
+++ js/view/snake-body.js
@@ -1,93 +1,93 @@
 class SnakeBodyView {
     constructor(part) {
-        this.part = part;
+        this.x = part.x;
+        this.y = part.y;
+        this.color = part.color;
+
+        this.from = new Direction(
+            part.prev.x - part.x,
+            part.prev.y - part.y
+        );
+
+        this.to = new Direction(
+            part.next.x - part.x,
+            part.next.y - part.y
+        );
 
         game.screen.add(this);
     }
 
     draw(ctx) {
-        const part = this.part;
-
-        const from = new Direction(
-            part.prev.x - part.x,
-            part.prev.y - part.y
-        );
-
-        const to = new Direction(
-            part.next.x - part.x,
-            part.next.y - part.y
-        );
-
         ctx.save();
-        ctx.translate(part.x * 20 + 10, part.y * 20 + 10);
+        ctx.translate(this.x * 20 + 10, this.y * 20 + 10);
 
         for (let i = 2; i--;) {
             const width = 6 + i;
 
-            const isStraight = from.isOpposite(to);
+            const isStraight = this.from.isOpposite(this.to);
 
             let gradient;
             if (isStraight) {
                 gradient = ctx.createLinearGradient(
-                    from.cw().x * 11,
-                    from.cw().y * 11,
-                    to.cw().x * 11,
-                    to.cw().y * 11,
+                    this.from.cw().x * 11,
+                    this.from.cw().y * 11,
+                    this.to.cw().x * 11,
+                    this.to.cw().y * 11,
                 );
             } else {
                 gradient = ctx.createRadialGradient(
-                    (from.x || to.x) * 10,
-                    (from.y || to.y) * 10,
+                    (this.from.x || this.to.x) * 10,
+                    (this.from.y || this.to.y) * 10,
                     0,
-                    (from.x || to.x) * 10,
-                    (from.y || to.y) * 10,
+                    (this.from.x || this.to.x) * 10,
+                    (this.from.y || this.to.y) * 10,
                     20
                 );
             }
-            gradient.addColorStop(0, i ? '#eb6' : ['#f22','#0c0','#22f','#aa0'][part.color]);
-            gradient.addColorStop(.5, i ? '#fc8' : ['#faa','#afa','#aaf','#ff8'][part.color]);
-            gradient.addColorStop(1, i ? '#eb6' : ['#f22','#0c0','#22f','#aa0'][part.color]);
+            gradient.addColorStop(0, i ? '#eb6' : ['#f22','#0c0','#22f','#aa0'][this.color]);
+            gradient.addColorStop(.5, i ? '#fc8' : ['#faa','#afa','#aaf','#ff8'][this.color]);
+            gradient.addColorStop(1, i ? '#eb6' : ['#f22','#0c0','#22f','#aa0'][this.color]);
 
             ctx.fillStyle = gradient;
 
             ctx.beginPath();
             ctx.moveTo(
-                from.x * 10 + from.ccw().x * width,
-                from.y * 10 + from.ccw().y * width
+                this.from.x * 10 + this.from.ccw().x * width,
+                this.from.y * 10 + this.from.ccw().y * width
             );
             if (isStraight) {
                 ctx.lineTo(
-                    to.x * 10 + to.cw().x * width,
-                    to.y * 10 + to.cw().y * width
+                    this.to.x * 10 + this.to.cw().x * width,
+                    this.to.y * 10 + this.to.cw().y * width
                 );
             } else {
                 ctx.quadraticCurveTo(
-                    (from.ccw().x || to.cw().x) * width,
-                    (from.ccw().y || to.cw().y) * width,
-                    to.x * 10 + to.cw().x * width,
-                    to.y * 10 + to.cw().y * width
+                    (this.from.ccw().x || this.to.cw().x) * width,
+                    (this.from.ccw().y || this.to.cw().y) * width,
+                    this.to.x * 10 + this.to.cw().x * width,
+                    this.to.y * 10 + this.to.cw().y * width
                 );
             }
             ctx.lineTo(
-                to.x * 10 + to.ccw().x * width,
-                to.y * 10 + to.ccw().y * width
+                this.to.x * 10 + this.to.ccw().x * width,
+                this.to.y * 10 + this.to.ccw().y * width
             );
             if (isStraight) {
                 ctx.lineTo(
-                    from.x * 10 + from.cw().x * width,
-                    from.y * 10 + from.cw().y * width
+                    this.from.x * 10 + this.from.cw().x * width,
+                    this.from.y * 10 + this.from.cw().y * width
                 );
             } else {
                 ctx.quadraticCurveTo(
-                    (from.cw().x || to.ccw().x) * width,
-                    (from.cw().y || to.ccw().y) * width,
-                    from.x * 10 + from.cw().x * width,
-                    from.y * 10 + from.cw().y * width
+                    (this.from.cw().x || this.to.ccw().x) * width,
+                    (this.from.cw().y || this.to.ccw().y) * width,
+                    this.from.x * 10 + this.from.cw().x * width,
+                    this.from.y * 10 + this.from.cw().y * width
                 );
             }
             ctx.fill();
 
-            if (part.color === undefined) break;
+            if (this.color === undefined) break;
         }
 
         ctx.restore();
