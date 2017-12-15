e9a9e15a9b8b051c10b6b24b611ff46075aa87c3
Becky <becky@monometric.net>
2017-12-16 00:16:16 +0100

    refactor

--- js/view/snake-tail.js
+++ js/view/snake-tail.js
@@ -1,33 +1,32 @@
 class SnakeTailView {
     constructor(part) {
-        this.part = part;
+        this.x = part.x;
+        this.y = part.y;
         this.z = 5;
+        this.from = new Direction(
+            part.prev.x - part.x,
+            part.prev.y - part.y
+        );
+        this.part = part;
 
         game.screen.add(this);
     }
 
     move(part) {
-        this.part = part;
-    }
-
-    draw(ctx) {
-        const part = this.part;
-
-        ctx.save();
-        ctx.translate(part.x * 20, part.y * 20);
-
-        const from = part.prev && new Direction(
+        this.x = part.x;
+        this.y = part.y;
+        this.from = new Direction(
             part.prev.x - part.x,
             part.prev.y - part.y
         );
+    }
 
-        const to = part.next && new Direction(
-            part.next.x - part.x,
-            part.next.y - part.y
-        );
+    draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x * 20, this.y * 20);
 
         ctx.translate(10, 10);
-        ctx.rotate(from.toAngle() * Math.PI / 2);
+        ctx.rotate(this.from.toAngle() * Math.PI / 2);
         ctx.scale(2, 1);
 
         ctx.beginPath();
@@ -41,26 +40,6 @@ class SnakeTailView {
         gradient.addColorStop(1, '#eb6');
         ctx.fillStyle = gradient;
         ctx.fill();
-
-        if (part.prev.color !== undefined) {
-            ctx.scale(.4, 1);
-            ctx.beginPath();
-            ctx.arc(-12.5, 0, 6, Math.PI * .5, Math.PI * 1.5, 1);
-
-            const gradient = ctx.createRadialGradient(
-                -12.5,
-                0,
-                0,
-                -12.5,
-                0,
-                10
-            );
-            gradient.addColorStop(0, ['#faa','#afa','#aaf','#ff8'][part.prev.color]);
-            gradient.addColorStop(1, ['#f22','#0c0','#22f','#aa0'][part.prev.color]);
-
-            ctx.fillStyle = gradient;
-            ctx.fill();
-        }
 
         ctx.restore();
     }
