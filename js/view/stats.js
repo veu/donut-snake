b5cc1cc050fdb56d0c05da7384dcda30a0475716
Becky <becky@monometric.net>
2017-12-19 18:48:10 +0100

    show when low on moves

--- js/view/stats.js
+++ js/view/stats.js
@@ -1,21 +1,27 @@
 class StatsView {
     constructor() {
         this.z = 10;
+        this.animationStep = 0;
         game.screen.add(this);
     }
 
     draw(ctx) {
+        ++ this.animationStep;
+
         ctx.fillStyle = '#f6b';
         ctx.fillRect(-20, 110, 140, 200);
         ctx.fillStyle = '#200';
         let offset = 125;
 
-        const {moves, score, highScore} = game.state;
+        const {moves, score, highScore, snake} = game.state;
+        const isInDanger = (moves + snake.colors.length < 5 || moves < 3) && moves > 0 && !game.locked;
+
         for (const [key, value] of [['Moves', moves], ['Score', score], ['High Score', highScore]]) {
             let leftWidth = ctx.measureText(key).width;
             let rightWidth = ctx.measureText(value).width;
             for (let i = 0; i < 96 - leftWidth - rightWidth; i += 2) {
-                ctx.fillRect(3 + leftWidth + i, offset, 1, 1);
+                const elementOffset = key == 'Moves' && isInDanger ? offset + Math.sin(i + this.animationStep) / 3 : offset;
+                ctx.fillRect(3 + leftWidth + i, elementOffset, 1, 1);
             }
             ctx.textAlign = 'left';
             ctx.fillText(key, 2, offset + 1);
