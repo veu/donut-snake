10a823fae7c0366b1f1b6046f2dee7c4b2a7df09
Becky <becky@monometric.net>
2018-01-05 20:08:10 +0100

    nerf power-up

--- js/grid.js
+++ js/grid.js
@@ -57,7 +57,7 @@ class Grid {
         const color = this.getRandomColor(x, y);
         game.state.grid[x + y * 5] = color;
 
-        if (sprinkle && game.state.sprinkles === null && color < 4 && !game.powerUp.isActive() && Math.random() < .2) {
+        if (sprinkle && game.state.sprinkles === null && color < 4 && !game.powerUp.isActive() && Math.random() < .1) {
             game.state.sprinkles = x + y * 5;
         }
 
